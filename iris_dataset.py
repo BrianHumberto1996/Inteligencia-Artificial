96e83073210b907225b93f967f47820da2338e5b
BrianHumberto1996 <31777289+BrianHumberto1996@users.noreply.github.com>
2018-03-21 08:49:32 -0600

    Update iris_dataset.py

--- iris_dataset.py
+++ iris_dataset.py
@@ -4,33 +4,33 @@
 """
 =========================================================
 The Iris Dataset
-=========================================================
-This data sets consists of 3 different types of irises'
-(Setosa, Versicolour, and Virginica) petal and sepal
-length, stored in a 150x4 numpy.ndarray
+=============================================== =======
+Este conjunto de datos consta de 3 tipos diferentes de iris
+(Setosa, Versicolour y Virginica) pétalo y sépalo
+de longitud, almacenado en un nuzzy.
 
-The rows being the samples and the columns being:
-Sepal Length, Sepal Width, Petal Length	and Petal Width.
+Las filas son las muestras y las columnas que son:
+Longitud del sepal, ancho del sepal, longitud del pétalo y ancho del pétalo.
 
-The below plot uses the first two features.
-See `here <https://en.wikipedia.org/wiki/Iris_flower_data_set>`_ for more
-information on this dataset.
+La gráfica de abajo usa las dos primeras características.
+Consulte `aquí <https://en.wikipedia.org/wiki/Iris_flower_data_set>` _ para obtener más información.
+información sobre este conjunto de datos.
 """
 print(__doc__)
 
 
-# Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
+# Fuente de codigo: Gaël Varoquaux
+#  Modificado para la documentación por Jaques Grobler
+# Licencia: BSD 3 clause
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
 from sklearn.decomposition import PCA
 
-# import some data to play with
+# Importar algunos datos para jugar 
 iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
+X = iris.data[:, :2]  # Nosotros solo tomamos dos caracteristicas
 y = iris.target
 
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
@@ -39,9 +39,8 @@ y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 plt.figure(2, figsize=(8, 6))
 plt.clf()
 
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
-            edgecolor='k')
+# Trazar los puntos de entrenamiento
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor='k')
 plt.xlabel('Sepal length')
 plt.ylabel('Sepal width')
 
@@ -50,8 +49,8 @@ plt.ylim(y_min, y_max)
 plt.xticks(())
 plt.yticks(())
 
-# To getter a better understanding of interaction of the dimensions
-# plot the first three PCA dimensions
+# Para obtener una mejor comprensión de la interacción de las dimensiones
+# Traza las primeras tres dimenciones de PCA
 fig = plt.figure(1, figsize=(8, 6))
 ax = Axes3D(fig, elev=-150, azim=110)
 X_reduced = PCA(n_components=3).fit_transform(iris.data)
